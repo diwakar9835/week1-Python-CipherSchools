22beb471dc5ceada141513be3e3202e1367d16a2
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-11-28 23:13:55 +0530

    formatting

---
+++ formatting.py
@@ -0,0 +1,9 @@
+name = "Ankit"
+age = 24
+print("hello" + name + " your age is " + str(age)) # ugly syntax
+# string formatting
+
+print("hello {} your age is {} ".format(name, age +2)) # python 3
+
+# 3.6
+print(f"hello {name} your age is {age+2}")
