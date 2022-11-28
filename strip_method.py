4cf7323ad6d0dc6e9b8c2ae0bdded8a5afdc44c2
Ankit Diwakar <116998434+diwakar9835@users.noreply.github.com>
2022-11-29 01:01:26 +0530

    strip_method

---
+++ strip_method.py
@@ -0,0 +1,8 @@
+name = "    Harshit    "
+dots = "............"
+# 1strip() method
+print(name + dots)
+print(name.lstrip() + dots)
+print(name.rstrip() + dots)
+print(name.strip() + dots)
+print(name.replace(" ", "") + dots)
